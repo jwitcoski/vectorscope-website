bd6733bb9a025ec0f4a4bca10681c90d63082a46
Jonathan Witcoski <jwitcoski@gmail.com>
2025-06-19 14:08:16 -0400

    added some files

--- iam.tf
+++ iam.tf
@@ -1,12 +1,6 @@
-# GitHub OIDC Provider
-resource "aws_iam_openid_connect_provider" "github" {
+# Data source for existing GitHub OIDC Provider
+data "aws_iam_openid_connect_provider" "github" {
   url = "https://token.actions.githubusercontent.com"
-
-  client_id_list = ["sts.amazonaws.com"]
-
-  thumbprint_list = [
-    "6938fd4d98bab03faadb97b34396831e3780aea1"  # GitHub's OIDC thumbprint
-  ]
 }
 
 # IAM Role for GitHub Actions
@@ -20,7 +14,7 @@ resource "aws_iam_role" "github_actions" {
         Action = "sts:AssumeRoleWithWebIdentity"
         Effect = "Allow"
         Principal = {
-          Federated = aws_iam_openid_connect_provider.github.arn
+          Federated = data.aws_iam_openid_connect_provider.github.arn
         }
         Condition = {
           StringLike = {
@@ -43,19 +37,50 @@ resource "aws_iam_role_policy" "github_actions_policy" {
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
-      # S3 permissions for website bucket
+      # IAM permissions for creating and managing OIDC providers and roles
       {
         Effect = "Allow"
         Action = [
+          "iam:CreateOpenIDConnectProvider",
+          "iam:GetOpenIDConnectProvider",
+          "iam:DeleteOpenIDConnectProvider",
+          "iam:CreateRole",
+          "iam:GetRole",
+          "iam:PutRolePolicy",
+          "iam:GetRolePolicy",
+          "iam:DeleteRolePolicy",
+          "iam:DeleteRole",
+          "iam:AttachRolePolicy",
+          "iam:DetachRolePolicy",
+          "iam:PassRole"
+        ]
+        Resource = "*"
+      },
+      # S3 permissions for website bucket (including creation)
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:CreateBucket",
+          "s3:DeleteBucket",
+          "s3:GetBucketLocation",
+          "s3:GetBucketPolicy",
+          "s3:PutBucketPolicy",
+          "s3:DeleteBucketPolicy",
+          "s3:PutBucketWebsite",
+          "s3:GetBucketWebsite",
+          "s3:DeleteBucketWebsite",
+          "s3:PutBucketPublicAccessBlock",
+          "s3:GetBucketPublicAccessBlock",
           "s3:PutObject",
           "s3:GetObject",
           "s3:ListBucket",
           "s3:DeleteObject",
-          "s3:PutObjectAcl"
+          "s3:PutObjectAcl",
+          "s3:GetObjectAcl"
         ]
         Resource = [
-          aws_s3_bucket.website.arn,
-          "${aws_s3_bucket.website.arn}/*"
+          "arn:aws:s3:::vectorscopeai.com",
+          "arn:aws:s3:::vectorscopeai.com/*"
         ]
       },
       # S3 permissions for Terraform state bucket (including creation)
@@ -63,10 +88,16 @@ resource "aws_iam_role_policy" "github_actions_policy" {
         Effect = "Allow"
         Action = [
           "s3:CreateBucket",
+          "s3:DeleteBucket",
           "s3:ListBucket",
           "s3:GetBucketLocation",
           "s3:PutBucketVersioning",
-          "s3:PutBucketEncryption"
+          "s3:GetBucketVersioning",
+          "s3:PutBucketEncryption",
+          "s3:GetBucketEncryption",
+          "s3:GetBucketPolicy",
+          "s3:PutBucketPolicy",
+          "s3:DeleteBucketPolicy"
         ]
         Resource = [
           "arn:aws:s3:::vectorscope-terraform-state"
@@ -77,45 +108,40 @@ resource "aws_iam_role_policy" "github_actions_policy" {
         Action = [
           "s3:GetObject",
           "s3:PutObject",
-          "s3:DeleteObject"
+          "s3:DeleteObject",
+          "s3:GetObjectVersion",
+          "s3:DeleteObjectVersion"
         ]
         Resource = [
           "arn:aws:s3:::vectorscope-terraform-state/*"
         ]
       },
-      # CloudFront permissions
+      # CloudFront permissions (including creation)
       {
         Effect = "Allow"
         Action = [
+          "cloudfront:CreateDistribution",
+          "cloudfront:GetDistribution",
+          "cloudfront:UpdateDistribution",
+          "cloudfront:DeleteDistribution",
+          "cloudfront:ListDistributions",
           "cloudfront:CreateInvalidation",
           "cloudfront:GetInvalidation",
           "cloudfront:ListInvalidations",
-          "cloudfront:GetDistribution",
-          "cloudfront:ListDistributions"
+          "cloudfront:CreateCloudFrontOriginAccessIdentity",
+          "cloudfront:GetCloudFrontOriginAccessIdentity",
+          "cloudfront:UpdateCloudFrontOriginAccessIdentity",
+          "cloudfront:DeleteCloudFrontOriginAccessIdentity",
+          "cloudfront:ListCloudFrontOriginAccessIdentities"
         ]
-        Resource = [
-          aws_cloudfront_distribution.website.arn,
-          "arn:aws:cloudfront::*:distribution/*"
-        ]
-      },
-      # IAM permissions for managing the role itself
-      {
-        Effect = "Allow"
-        Action = [
-          "iam:GetRole",
-          "iam:GetRolePolicy",
-          "iam:PutRolePolicy",
-          "iam:DeleteRolePolicy"
-        ]
-        Resource = [
-          aws_iam_role.github_actions.arn
-        ]
+        Resource = "*"
       },
       # EC2 permissions for Terraform
       {
         Effect = "Allow"
         Action = [
-          "ec2:DescribeRegions"
+          "ec2:DescribeRegions",
+          "ec2:DescribeAvailabilityZones"
         ]
         Resource = "*"
       }
